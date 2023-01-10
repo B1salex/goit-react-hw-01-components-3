9fbe958df536c943134a660fb03893e0540564e8
Alex Mandebura <xenovidbis@gmail.com>
2023-01-10 15:38:35 +0200

    err

--- src/components/Statistics/RandomColor.js
+++ src/components/Statistics/RandomColor.js
@@ -1,3 +1,5 @@
-export default function randomColor () {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+export default function randomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+//goo
